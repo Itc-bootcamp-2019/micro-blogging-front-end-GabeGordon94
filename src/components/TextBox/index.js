834971d1d7705e31aa4bd040e4594bee043397a5
Gabriel Gordon <gabegor94@gmail.com>
2019-12-17 12:22:10 +0200

    added error with local storage if you  dont have a username

--- src/components/TextBox/index.js
+++ src/components/TextBox/index.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import {Link} from 'react-router-dom'
 
 class TextBox extends React.Component {
     constructor(props) {
         super(props);
-        let userName=localStorage.getItem('username');
+        this.userName = localStorage.getItem('username');
         this.state = {
-            name: userName,
+            name: this.userName,
             text: ''
         }
     }
@@ -37,14 +38,27 @@ class TextBox extends React.Component {
 
     render() {
         const { name, text } = this.state;
+
         return (
             <div className="w-100 justify-content-center d-flex">
-                <textarea type='text' id="newTweetBox" placeholder="What you have in mind..." maxLength='140'
-                    onChange={(event) => this.validator(event)} />
-                <button id="tweetButton" className="btn" onClick={() =>
-                    this.props.onClick(name, text, this.getDate())}
-                >Tweet</button>
-                <span id="exceedingLength" className="d-none">The tweet can't contain more then 140 chars.</span>
+                {this.userName &&
+                    <div className="w-100 justify-content-center d-flex">
+                        <textarea type='text' id="newTweetBox" placeholder="What you have in mind..." maxLength='140'
+                            onChange={(event) => this.validator(event)} />
+                        <button id="tweetButton" className="btn" onClick={() =>
+                            this.props.onClick(name, text, this.getDate())}
+                        >Tweet</button>
+                        <span id="exceedingLength" className="d-none">The tweet can't contain more then 140 chars.</span>
+                    </div>
+                }
+                {!this.userName &&
+                    <>
+                       <Link to="/profile"className="text-danger"> <h1>Set Your Username</h1></Link>
+                        <button id="tweetButton" className="btn d-none" onClick={() =>
+                            this.props.onClick(name, text, this.getDate())}
+                        >Tweet</button>
+                    </>
+                }
             </div>
         );
     }
